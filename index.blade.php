63eff3952a1601e9e0e4a68a903516da35bacea3
thuandquang <55191305+thuandquang@users.noreply.github.com>
2019-12-06 21:18:05 +0700

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Font aowd -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+</head>
+
+<body>
+
+
+    <div class="container mt-2">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-4 mt-4 d-flex">
+                <img class="mb-4 mx-1 border-right" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUoOwQ0SnSVqrakbM5xqYcoD10px2o9AqFaAAUWRTwBMrgghv0&s" style="width: 30px; height: 30px;">
+                <h3 class="mx-1" style="font-family:Bodoni ">Buzz</h3>
+            </div>
+            <div class="col-12 col-lg-4 d-flex">
+                <form action="#" class="search-form d-inline-block mt-2">
+                    <div class="d-flex">
+                        <input type="email" class="form-control" placeholder="Search...">
+                        <button type="submit" class="btn btn-dark"><i class="fas fa-search"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-12 col-lg-4 ml-auto d-flex">
+                <div class="ml-md-auto top-social d-none d-lg-inline-block">
+                    <a href="#" class="d-inline-block p-3 text-dark"><i class="fas fa-bullhorn fa-2px"></i></a>
+                    <a href="#" class="d-inline-block p-3 text-dark"><i class="fas fa-comment fa-2px"></i></a>
+                    <a href="#" class="d-inline-block p-3 text-dark"><i class="fas fa-user fa-2px"></i></a>
+                    <a href="#" class="d-inline-block p-3 text-dark"><i class="fas fa-cog fa-2px"></i></a>
+
+
+                </div>
+
+            </div>
+
+        </div>
+        <hr>
+        <div class="row justify-content-center my-3" style="width:100%">
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-dark">Bài viết</button>
+
+                <button type="button" class="btn btn-dark">Mua hàng</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="first-row justify-content-center mx-4">
+            <div class="card text-white align-items-center" style="width:100%">
+                <img src="https://cdn.thesolesupplier.co.uk/2019/04/THE-TEN.jpg" style=" width:100% ">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Chào đón sản phẩm giữa Nike x Off-white</h5>
+                    <p class="card-text">Collection 10 đôi giày siêu kinh điển.</p>
+                    <p class="card-text">Last updated 3 mins ago</p>
+                    <button>Xem thêm >></button>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="second-row d-flex mt-4">
+            <div class="col-3">
+                <div>
+                    <img src="https://hnbmg.com/wp-content/uploads/air-jordan-1-dior-release-info-6-1024x719.jpg" style="width: 100%">
+                </div>
+                <div>
+                    <div class="d-flex justify-content-between my-1">
+                        <span><small>April 10, 2019</small></span>
+                        <span><small>by HolyFirst</small></span>
+                    </div>
+                    <center>
+                        <h3>Nike tung siêu phẩm DIOR x Air Jordan 1</h3>
+                    </center>
+                    <button>Xem thêm >></button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div>
+                    <img src="https://hnbmg.com/wp-content/uploads/air-jordan-13-chinese-new-year-CW4409-100-0-1024x719.jpg" style="width: 100%">
+                </div>
+                <div>
+                    <div class="d-flex justify-content-between my-1">
+                        <span><small>April 10, 2019</small></span>
+                        <span><small>by HolyFirst</small></span>
+                    </div>
+                    <center>
+                        <h3>Đón 2020 cùng Air Jordan 13 “Chinese New Year”</h3>
+                    </center>
+                    <button>Xem thêm >></button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div>
+                    <img src="https://hnbmg.com/wp-content/uploads/Nike-Shox-Nova-Pink-Orange-AT8046_002-3-1024x719.jpg" style="width: 100%">
+                </div>
+                <div>
+                    <div class="d-flex justify-content-between my-1">
+                        <span><small>April 10, 2019</small></span>
+                        <span><small>by HolyFirst</small></span>
+                    </div>
+                    <center>
+                        <h3>Nike Shox Nova và phối màu tươi rói độc quyền cho nữ</h3>
+                    </center>
+                    <button>Xem thêm >></button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div>
+                    <img src="https://hnbmg.com/wp-content/uploads/viotech-nike-dunks-CT5050-500-1-1024x719.jpg" style="width: 100%">
+                </div>
+                <div>
+                    <div class="d-flex justify-content-between my-1">
+                        <span><small>April 10, 2019</small></span>
+                        <span><small>by HolyFirst</small></span>
+                    </div>
+                    <center>
+                        <h3>Nike Dunk Low "Viotech" & tái bản thứ 3 vào tuần tới</h3>
+                    </center>
+                    <button>Xem thêm >></button>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+        <div class="third-row mt-4" style="width:100%">
+            <div class="card mb-3" style="width: 100%">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="https://hnbmg.com/wp-content/uploads/Thumb-3.jpg" class="card-img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Vans quyên góp hơn 1 triệu USD</h5>
+                            <p class="card-text">Vans Thành Công Quyên Góp Hơn 1 Triệu USD Và Thành Lập Quỹ Vans Checkerboard Để Tiếp Năng Lượng Cho Sự Sáng Tạo.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <button>Xem thêm >></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="width: 100%">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="https://hnbmg.com/wp-content/uploads/Thumb-3.jpg" class="card-img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Vans quyên góp hơn 1 triệu USD</h5>
+                            <p class="card-text">Vans Thành Công Quyên Góp Hơn 1 Triệu USD Và Thành Lập Quỹ Vans Checkerboard Để Tiếp Năng Lượng Cho Sự Sáng Tạo.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <button>Xem thêm >></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="width: 100%">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="https://hnbmg.com/wp-content/uploads/Thumb-3.jpg" class="card-img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Vans quyên góp hơn 1 triệu USD</h5>
+                            <p class="card-text">Vans Thành Công Quyên Góp Hơn 1 Triệu USD Và Thành Lập Quỹ Vans Checkerboard Để Tiếp Năng Lượng Cho Sự Sáng Tạo.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <button>Xem thêm >></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="width: 100%">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="https://hnbmg.com/wp-content/uploads/Thumb-3.jpg" class="card-img">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Vans quyên góp hơn 1 triệu USD</h5>
+                            <p class="card-text">Vans Thành Công Quyên Góp Hơn 1 Triệu USD Và Thành Lập Quỹ Vans Checkerboard Để Tiếp Năng Lượng Cho Sự Sáng Tạo.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <button>Xem thêm >></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <hr>
+    <div class="container-fluid bg-dark">
+        <div class="row mx-2">
+            <div class="col-4">
+               
+                <ul class="d-flex flex-column list-unstyled mt-2">
+                    <li>
+                        <a class="text-white" href="">Tài khoản</a>
+                    </li>
+                    <li>
+                        <a class="text-white" href="">Giới thiệu</a>
+                    </li>
+                    <li>
+                        <a class="text-white" href="">Home</a>
+                    </li>
+                    <li>
+                        <a class="text-white" href="">Liên hệ</a>
+                    </li>
+
+            </div>
+            <div class="col-4">
+               
+                <ul class="d-flex flex-column list-unstyled mt-2">
+                    <li>
+                        <a class="text-white" href="">Tài khoản</a>
+                    </li>
+                    <li>
+                        <a class="text-white" href="">Giới thiệu</a>
+                    </li>
+                    <li>
+                        <a class="text-white" href="">Home</a>
+                    </li>
+                    <li>
+                        <a class="text-white" href="">Liên hệ</a>
+                    </li>
+
+            </div>
+            <div class="col-4 text-white">
+                <p>CONNECT WITH US</p>
+                <a href="#" class="d-inline-block p-3 text-white"><i class="fab fa-facebook-square fa-2px"></i></a>
+                <a href="#" class="d-inline-block p-3 text-white"><i class="fab fa-twitter fa-2px"></i></a>
+                <a href="#" class="d-inline-block p-3 text-white"><i class="far fa-envelope fa-2px"></i></a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
