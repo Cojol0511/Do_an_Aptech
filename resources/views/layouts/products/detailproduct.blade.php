df6473c6a306f9e443a9040859098aed3f1b2e9e
VoXuanPhuc <phucpro746@gmail.com>
2020-03-03 10:51:42 -0800

    update

--- resources/views/layouts/products/detailproduct.blade.php
+++ resources/views/layouts/products/detailproduct.blade.php
@@ -8,24 +8,58 @@
 		<!-- slide hien thi cho san pham -->
 
 		<div class="col-5">
-			
-				<div class="carousel-inner">
 				<?php
 					$images = $product->images;
 				?>
-				@foreach($images as $image)
-					<div class="carousel-item active">
-						<img src="{{asset('image/'.$image->name_image)}}" class="d-block w-100" alt="...">
-					</div>
-				
-				@endforeach
-				</div>
-				<div class="my-5">
-				@foreach($images as $image)
-					
-						<img src="{{asset('image/'.$image->name_image)}}"  alt="..." height="50px" width="30%">
-				
-				@endforeach</div>
+
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+						<?php
+							$i =0;
+						?>
+						@foreach($images as $image)
+							
+							<li data-target="#carouselExampleIndicators" data-slide-to="{{$i}}"
+							@if( $i == 0 )
+							class="active"
+							@endif
+							>
+							</li>
+
+							<?php
+								$i++;
+							?>
+						@endforeach
+						</ol>
+						<div class="carousel-inner">
+						<?php
+							$i =0;
+						?>
+						@foreach($images as $image)
+							
+							<div 
+							@if($i ==0)
+							class="carousel-item active"
+							@else
+							class="carousel-item "
+							@endif
+							>
+							<img src="{{asset('image/'.$image->name_image)}}" class="d-block w-100" alt="...">
+							</div>
+							<?php
+								$i++;
+							?>
+						@endforeach
+						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+						</div>
 			<h1>Hình Ảnh Sản Phẩm</h1>
 		</div>
 		<div class="col-1"></div>
